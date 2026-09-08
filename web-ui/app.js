b23db4340441b2ddfe4715dc9a3f9689dcea402c
Armin <armin@weidtler.de>
2026-09-08 20:06:17 +0200

    add test user in frontend

--- web-ui/app.js
+++ web-ui/app.js
@@ -70,6 +70,10 @@ const api = {
       headers: {
         ...(options.headers || {}),
         'Content-Type': 'application/json',
+        'Remote-User': 'TestUser',
+        'Remote-Email': 'TestUser@Test.test',
+        'Remote-Name': 'TestUserName',
+        'Remote-Groups': 'TestGroup1, TestGroup2'
       },
     })
     if (!r.ok) {
